c4e7a65fbcf05d093765ffa7c6c80f747fc02028
jaesebaese <jj.hochuli@hotmail.com>
2026-04-17 16:03:12 +0200

    cleaned up unneeded code

--- graphtutorial/graph.py
+++ graphtutorial/graph.py
@@ -7,15 +7,6 @@ from configparser import SectionProxy
 from azure.identity import DeviceCodeCredential
 from msgraph import GraphServiceClient
 from msgraph.generated.users.item.user_item_request_builder import UserItemRequestBuilder
-from msgraph.generated.users.item.mail_folders.item.messages.messages_request_builder import (
-    MessagesRequestBuilder)
-from msgraph.generated.users.item.send_mail.send_mail_post_request_body import (
-    SendMailPostRequestBody)
-from msgraph.generated.models.message import Message
-from msgraph.generated.models.item_body import ItemBody
-from msgraph.generated.models.body_type import BodyType
-from msgraph.generated.models.recipient import Recipient
-from msgraph.generated.models.email_address import EmailAddress
 import requests
 
 class Graph:
@@ -55,46 +46,6 @@ class Graph:
         return user
     # </GetUserSnippet>
 
-    # <GetInboxSnippet>
-    async def get_inbox(self):
-        query_params = MessagesRequestBuilder.MessagesRequestBuilderGetQueryParameters(
-            # Only request specific properties
-            select=['from', 'isRead', 'receivedDateTime', 'subject'],
-            # Get at most 25 results
-            top=25,
-            # Sort by received time, newest first
-            orderby=['receivedDateTime DESC']
-        )
-        request_config = MessagesRequestBuilder.MessagesRequestBuilderGetRequestConfiguration(
-            query_parameters= query_params
-        )
-
-        messages = await self.user_client.me.mail_folders.by_mail_folder_id('inbox').messages.get(
-                request_configuration=request_config)
-        return messages
-    # </GetInboxSnippet>
-
-    # <SendMailSnippet>
-    async def send_mail(self, subject: str, body: str, recipient: str):
-        message = Message()
-        message.subject = subject
-
-        message.body = ItemBody()
-        message.body.content_type = BodyType.Text
-        message.body.content = body
-
-        to_recipient = Recipient()
-        to_recipient.email_address = EmailAddress()
-        to_recipient.email_address.address = recipient
-        message.to_recipients = []
-        message.to_recipients.append(to_recipient)
-
-        request_body = SendMailPostRequestBody()
-        request_body.message = message
-
-        await self.user_client.me.send_mail.post(body=request_body)
-    # </SendMailSnippet>
-
     def get_policy_with_settings(self, token, policy_id):
         headers = {"Authorization": f"Bearer {token}"}
         url = f"https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/{policy_id}/settings"
@@ -112,7 +63,7 @@ class Graph:
     # <MakeGraphCallSnippet>
     async def make_graph_call(self):
         token = await self.get_user_token()
-        url = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies"
+        url = "https://graph.microsoft.com/beta/deviceManagement/configurationSettings"
         headers = {"Authorization": f"Bearer {token}", "Accept": "application/json"}
 
         all_policies = []
@@ -123,17 +74,17 @@ class Graph:
             all_policies.extend(data.get("value", []))
             url = data.get("@odata.nextLink")
         
-        all_policies_with_settings = []
+        """ all_policies_with_settings = []
 
         for policy in all_policies:
             settings = self.get_policy_with_settings(token, policy.get("id"))
             policy["settings"] = settings
             all_policies_with_settings.append(policy)
-
+ """
 
         # Write all_policies to a new file
-        with open('policies_and_settings.json', 'w') as f:
-           json.dump(all_policies_with_settings, f, indent=4)
+        with open('policies_and_settings_expand.json', 'w') as f:
+           json.dump(all_policies, f, indent=4)
 
         return all_policies
     # </MakeGraphCallSnippet>
